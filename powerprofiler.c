ccd71d24badf22dfb167a26620bbbcc9a68aaeb7
konaoki <kon.aoki.us@gmail.com>
2018-06-19 12:55:00 -0600

    updated Makefile

--- powerprofiler.c
+++ powerprofiler.c
@@ -1,10 +1,8 @@
-#include "/home/kona/pwr2018/pwrapi-ref/src/pwr/pwr.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
 #include <unistd.h>
-
 
 #define INTERVAL_SEC 0
 #define INTERVAL_NANO 100000000
@@ -20,73 +18,70 @@ double value;
 
 int measure_children(PWR_Obj o)
 {
-  inite = 0;
-  if(PWR_ObjAttrIsValid(o,PWR_ATTR_ENERGY)==PWR_RET_SUCCESS)
-  {
-    int r;
-    //PWR_ObjAttrSetValue( self, PWR_ATTR_ENERGY, &inite);
-    r = PWR_ObjAttrGetValue( self, PWR_ATTR_ENERGY, &value, &ts );
-    char buffer[1000];
-    char name[100];
-    PWR_ObjGetName( o, name, 100 );
-    sprintf(buffer,"%f joules, %lu ms, %s\n",	value, ms_now(), name);
-    write_data(buffer,"ENERGY");
-  }
-  if(PWR_ObjAttrIsValid(o,PWR_ATTR_POWER)==PWR_RET_SUCCESS)
-  {
-    int r;
-    //PWR_ObjAttrSetValue( self, PWR_ATTR_POWER, &inite);
-    r = PWR_ObjAttrGetValue( self, PWR_ATTR_POWER, &value, &ts );
-    char buffer[1000];
-    char name[100];
-    PWR_ObjGetName( o, name, 100 );
-    sprintf(buffer,"%f watts, %lu ms, %s\n",	value, ms_now(), name);
-    write_data(buffer,"POWER");
+	inite = 0;
+	if(PWR_ObjAttrIsValid(o,PWR_ATTR_ENERGY)==PWR_RET_SUCCESS)
+	{
+		int r;
+		//PWR_ObjAttrSetValue( self, PWR_ATTR_ENERGY, &inite);
+		r = PWR_ObjAttrGetValue( self, PWR_ATTR_ENERGY, &value, &ts );
+		char buffer[1000];
+		char name[100];
+		PWR_ObjGetName( o, name, 100 );
+		sprintf(buffer,"%f joules, %lu ms, %s\n", value, ms_now(), name);
+		write_data(buffer,"ENERGY");
+	}
+	if(PWR_ObjAttrIsValid(o,PWR_ATTR_POWER)==PWR_RET_SUCCESS)
+	{
+		int r;
+		//PWR_ObjAttrSetValue( self, PWR_ATTR_POWER, &inite);
+		r = PWR_ObjAttrGetValue( self, PWR_ATTR_POWER, &value, &ts );
+		char buffer[1000];
+		char name[100];
+		PWR_ObjGetName( o, name, 100 );
+		sprintf(buffer,"%f watts, %lu ms, %s\n", value, ms_now(), name);
+		write_data(buffer,"POWER");
 
-  }
+	}
 
-  PWR_ObjType objType;
-  PWR_ObjGetType( o, &objType );
-  if(objType==PWR_OBJ_CORE)
-    return 0;
-  else
-  {
-    PWR_Grp children;
-    PWR_ObjGetChildren( o, &children );
-    for (int i = 0; i < PWR_GrpGetNumObjs(children); i++ ) {
-      PWR_Obj child;
-        PWR_GrpGetObjByIndx( children, i, &child );
-      return measure_children(child);
-    }
-  }
+	PWR_ObjType objType;
+	PWR_ObjGetType( o, &objType );
+	if(objType==PWR_OBJ_CORE)
+		return 0;
+	else
+	{
+		PWR_Grp children;
+		PWR_ObjGetChildren( o, &children );
+		for (int i = 0; i < PWR_GrpGetNumObjs(children); i++ ) {
+			PWR_Obj child;
+			PWR_GrpGetObjByIndx( children, i, &child );
+			return measure_children(child);
+		}
+		return 1;
+	}
 }
 int measure_energy()
 {
-  PWR_ObjType objType;
-  PWR_ObjGetType( self, &objType );
-  //set self to highest level in hierarchy
-  while(objType!=PWR_OBJ_PLATFORM)
-  {
-    PWR_ObjGetParent( self, &self );
-    PWR_ObjGetType( self, &objType );
-  }
-  //printf("set self to platform\n");
-  measure_children(self);
-  return 0;
+	PWR_ObjType objType;
+	PWR_ObjGetType( self, &objType );
+	//set self to highest level in hierarchy
+	while(objType!=PWR_OBJ_PLATFORM)
+	{
+		PWR_ObjGetParent( self, &self );
+		PWR_ObjGetType( self, &objType );
+	}
+	//recursively take measurements of children
+	measure_children(self);
+	return 0;
 }
 void *power_measurement(void *arg)
 {
-  //fprintf(logfile,"monitoring starts in power profiler %lu\n", ms_now()/1000);
-  // Get a context
-  rc = PWR_CntxtInit( PWR_CNTXT_DEFAULT, PWR_ROLE_APP, "App", &cntxt );
-  rc = PWR_CntxtGetEntryPoint( cntxt, &self );
+	// Get a context
+	rc = PWR_CntxtInit( PWR_CNTXT_DEFAULT, PWR_ROLE_APP, "App", &cntxt );
+	rc = PWR_CntxtGetEntryPoint( cntxt, &self );
 
-  while(monitoring)
-  {
-    //printf("measuring\n");
-    measure_energy();
-    sleep_abs(INTERVAL_NANO);
-  }
-  //fprintf(logfile,"monitoring ends in power profiler %lu\n", ms_now()/1000);
-
+	while(monitoring)
+	{
+		measure_energy();
+		sleep_abs(INTERVAL_NANO);
+	}
 }
--- profiler.c
+++ profiler.c
@@ -5,10 +5,12 @@
 #include <dlfcn.h>
 #include <pthread.h>
 #include <assert.h>
+#include <sys/mman.h>
 
 static int monitoring = 1;
 static int writing = 1;
 
+#include "pwr.h"
 #include "time.c"
 #include "file_streamer.c"
 #include "powerprofiler.c"
@@ -16,73 +18,57 @@ static int writing = 1;
 
 int main()
 {
-  return 0;
+	return 0;
 }
 
 pthread_t powThread;
 int profrank;
+int init()
+{
+	MPI_Comm_rank(MPI_COMM_WORLD, &profrank);
+	char hostname[64];
+	gethostname(hostname,64);
+	char fname[100];
+	sprintf(fname, "%s.dat", hostname);
+	int start_rc = start_write(fname);
+
+}
 int MPI_Init_thread(int *argc, char ***argv, int required, int *provided)
 {
- int r;
- // r=PMPI_Init(argc, argv);
-r= PMPI_Init_thread(argc,argv,required,provided);
-
-  MPI_Comm_rank(MPI_COMM_WORLD, &profrank);
-//  printf("my rank is %i\n",profrank);
-  char hostname[64];
-  gethostname(hostname,64);
-  char fname[100];
-  sprintf(fname, "%s.dat", hostname);
-  int start_rc = start_write(fname);
-//printf("start rc: %i\n",start_rc);
-//fflush(stdout);
-  //printf("starting write\n");
-  //fprintf(logfile, "start %lu\n", ms_now()/1000);
-  if(profrank==0)
-  {
-    pthread_create(&powThread, NULL, power_measurement, NULL);
-    //fprintf(logfile,"monitoring starts in init %lu\n", ms_now()/1000);
-  }
-return r;
+	int r;
+	r= PMPI_Init_thread(argc,argv,required,provided);
+	init();
+	if(profrank==0)
+	{
+		pthread_create(&powThread, NULL, power_measurement, NULL);
+	}
+	return r;
 }
 int MPI_Init(int *argc, char ***argv)
 {
-  int r;
- // r=PMPI_Init(argc, argv);
-r=PMPI_Init(argc,argv);
+	int r;
+	r=PMPI_Init(argc,argv);
+	init();
+	if(profrank==0)
+	{
+		pthread_create(&powThread, NULL, power_measurement, NULL);
+	}
 
-  MPI_Comm_rank(MPI_COMM_WORLD, &profrank);
-  //printf("my rank is %i\n",profrank);
-  char hostname[64];
-  gethostname(hostname,64);
-  char fname[100];
-  sprintf(fname, "%s.dat", hostname);
-  int start_rc = start_write(fname);
-//printf("start rc: %i\n",start_rc);
-//fflush(stdout);
-  //printf("starting write\n");
-  //fprintf(logfile, "start %lu\n", ms_now()/1000);
-  if(profrank==0)
-  {
-    pthread_create(&powThread, NULL, power_measurement, NULL);
-    //fprintf(logfile,"monitoring starts in init %lu\n", ms_now()/1000);
-  }
-
-  return r;
+	return r;
 }
 
 int MPI_Finalize()
 {
-  int r;
-  //printf("rank %i has finalized\n",profrank);
-  monitoring=0;
-  //fprintf(logfile,"monitoring ends in finalize %lu\n", ms_now()/1000);
-  if(profrank==0)
-  {
-    pthread_join(powThread,NULL);
-  }
-  //fprintf(logfile,"end %lu\n", ms_now()/1000);
-  end_write();
-  r=PMPI_Finalize();
-  return r;
+	int r;
+	//printf("rank %i has finalized\n",profrank);
+	monitoring=0;
+	//fprintf(logfile,"monitoring ends in finalize %lu\n", ms_now()/1000);
+	if(profrank==0)
+	{
+		pthread_join(powThread,NULL);
+	}
+	//fprintf(logfile,"end %lu\n", ms_now()/1000);
+	end_write();
+	r=PMPI_Finalize();
+	return r;
 }
